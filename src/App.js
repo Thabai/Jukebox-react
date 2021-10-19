cc080d7264bf82f3dbd1269f8faf3648c09ee20f
Carly Morris <thabai@hotmail.com>
2021-10-19 15:08:34 +0100

    first setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Albums from './Components/Albums.js';
+
+const App = () => {
+
+
+ return (
+ <div>
+ <Albums />
+ </div>
+ )
+}
+
+export default App;
---
+++ src/Pages/AlbumsAvailable.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Albums from './Components/Albums.js';
+
+const AlbumsAvailable = () => {
+
+
+ return (
+ <div>
+ <Albums />
+ </div>
+ )
+}
+
+export default AlbumsAvailable;
